2f15b0c33b1684d72f6fcd91d4ed7645a238681d
YassSSH <yassine.regragui@mediaschool.me>
2021-10-14 11:31:55 +0200

    POO TD 1

---
+++ Stagiaire.php
@@ -0,0 +1,43 @@
+<?php
+class Stagiaire {
+private string $_nom;
+private  array  $_notes;
+
+public function __construct(string $_nom , array $_notes){
+    $this->_nom = $_nom;
+    $this->_notes = $_notes;
+}
+
+    /**
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->_nom;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom(string $nom): void
+    {
+        $this->_nom = $nom;
+    }
+
+    /**
+     * @return array
+     */
+    public function getNotes(): array
+    {
+        return $this->_notes;
+    }
+
+    /**
+     * @param array $notes
+     */
+    public function setNotes(array $notes): void
+    {
+        $this->_notes = $notes;
+    }
+
+}
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
+<?php
 
+include ("./Point.php");
+include ("./TroisPoints.php");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,29 +16,65 @@
 <form action="index.php" method="post">
     <p>A : <input type="text" name="Aord" /> <input type="text" name="Aabs" /></p>
     <p>B : <input type="text" name="Bord" /> <input type="text" name="Babs" /></p>
-    <p>C : <input type="text" name="Cord" /> <input type="text" name="Cabs" /></p>
+    <p>C : <input type="text" name="Cord" /> <input  type="text" name="Cabs" value="<?php echo  $_POST['Cabs']?>"  /></p>
     <p><input type="submit" value="OK"></p>
 </form>
 </body>
 </html>
 
 <?php
-include ("./Point.php");
-include ("./TroisPoints.php");
+if(!empty($_POST['Aord']) && !empty($_POST['Aabs']) && !empty($_POST['Bord']) && !empty($_POST['Babs']) && !empty($_POST['Cord']) && !empty($_POST['Cabs']))
+{
+
+    function iso(){
+        $p1 = new Point($_POST['Aord'] , $_POST['Aabs']);
+        $p2 = new Point($_POST['Bord'] , $_POST['Babs']);
+        $p3 = new Point($_POST['Cord'] , $_POST['Cabs']);
+
+    echo $p1 , $p2 , $p3;
+        $var = new TroisPoints($p1, $p2, $p3);
+        if ($var->isocele()){
+            echo '<p> IL EST ISOCELE</p>';
+        }else{
+            echo '<p> IL EST PAS ISOCELE</p>';
+        }
+
+}
+iso();
+    function align(){
+        $p1 = new Point($_POST['Aord'] , $_POST['Aabs']);
+        $p2 = new Point($_POST['Bord'] , $_POST['Babs']);
+        $p3 = new Point($_POST['Cord'] , $_POST['Cabs']);
+
+        $var = new TroisPoints($p1, $p2, $p3);
+        if ($var->sontAlignes()){
+            echo '<p> IL SONT ALIGNES</p>';
+        }else{
+            echo '<p> IL NE SONT PAS ALIGNES</p>';
+        }
+
+    }
+    align();
+
+
+    }
+
+
+
 
 
 function bool(){
-    $p1 = new Point($_POST['Aord'], $_POST['Aabs']);
-    $p2 = new Point('10', '10');
-    $p3 = new Point('10', '10');
+    $p1 = new Point('0', '12');
+    $p2 = new Point('180', '107');
+    $p3 = new Point('102', '10');
     $var = new TroisPoints($p1, $p2, $p3);
+$var->sontAlignes();
 
-    var_dump($var->sontAlignes());
 }
 bool();
 
 function bool1(){
-    $p1 = new Point('220', '10');
+    $p1 = new Point('20', '10');
     $p2 = new Point('02', '0');
     $p3 = new Point('1', '0');
     $var = new TroisPoints($p1, $p2, $p3);
